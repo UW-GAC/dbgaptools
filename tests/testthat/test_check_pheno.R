50a44e87ecf1223cd034d4acbcbc41b1a68f3663
Jai Broome <broome.jai@gmail.com>
2020-03-25 13:47:48 -0700

    fix space-after-comma linting error

--- tests/testthat/test_check_pheno.R
+++ tests/testthat/test_check_pheno.R
@@ -4,18 +4,18 @@ pheno_dd <- system.file("extdata", "2b_dbGaP_SubjectPhenotypesDD.xlsx", package 
 pheno_ds <- system.file("extdata", "2a_dbGaP_SubjectPhenotypesDS.txt", package = "dbgaptools", mustWork = TRUE)
 
 dd <- read_dd_file(pheno_dd)
-ds <- read_ds_file(pheno_ds, na_vals = c("NA", "N/A", "na", "n/a","9999"))
+ds <- read_ds_file(pheno_ds, na_vals = c("NA", "N/A", "na", "n/a", "9999"))
 
-test_that("Compliant files run error free",{
-  expect_null(check_pheno(dsfile = pheno_ds, na_vals = c("NA", "N/A", "na", "n/a","9999")))
+test_that("Compliant files run error free", {
+  expect_null(check_pheno(dsfile = pheno_ds, na_vals = c("NA", "N/A", "na", "n/a", "9999")))
 })
 
-test_that("Missing ID column stops with error",{
+test_that("Missing ID column stops with error", {
   str <- "Please check that dsfile contains column for subject-level ID"
   expect_error(check_pheno(pheno_ds, subjectID_col = "mysubject"), str, fixed = TRUE)  
 })
 
-test_that("Warning of non-preferred subject ID col is issued",{
+test_that("Warning of non-preferred subject ID col is issued", {
   ds.rev <- read_ds_file(pheno_ds)
   names(ds.rev)[1] <- "INDIVIDUAL_ID"
   ds.rev.fn <- tempfile(fileext = ".txt")
@@ -27,7 +27,7 @@ test_that("Warning of non-preferred subject ID col is issued",{
 })
 
 # non unique subject ID flags
-test_that("Non-unique SUBJECT_ID is reported",{
+test_that("Non-unique SUBJECT_ID is reported", {
   ds.rev <- read_ds_file(pheno_ds)
   ds.rev$SUBJECT_ID[4] <- ds.rev$SUBJECT_ID[3]
   ds.rev.fn <- tempfile(fileext = ".txt")
@@ -40,15 +40,15 @@ test_that("Non-unique SUBJECT_ID is reported",{
 
 test_that("Extra subjects are detected", {
   ds <- read_ds_file(pheno_ds)
-  subj_exp <- ds[,1]
+  subj_exp <- ds[, 1]
   subj_exp_less <- subj_exp[-c(3:4)]
   out <- check_pheno(pheno_ds, subj_exp = subj_exp_less)
-  expect_equal(out$extra_subjects, c("3","4"))  
+  expect_equal(out$extra_subjects, c("3", "4"))  
 })
 
 test_that("Missing subjects are detected", {
   ds <- read_ds_file(pheno_ds)
-  subj_exp <- ds[,1]
+  subj_exp <- ds[, 1]
   subj_exp_more <- rbind(subj_exp, "999")
   out <- check_pheno(pheno_ds, subj_exp = subj_exp_more)
   expect_equal(out$missing_subjects, c("999"))
