d98020f12f79eb74140d25715882cf36ca4b84d2
Adrienne Stilp <amstilp@uw.edu>
2020-10-26 17:34:54 -0700

    Fix failing DD tests with updated tidyverse packages

--- tests/testthat/test_check_dd.R
+++ tests/testthat/test_check_dd.R
@@ -1,6 +1,6 @@
 context("Checking data dictionary (DD) file")
 
-# use sample attributes as starting 
+# use sample attributes as starting
 ddfn <- system.file("extdata", "3b_dbGaP_SampleAttributesDD.xlsx", package = "dbgaptools", mustWork = TRUE)
 dsfn <- system.file("extdata", "3a_dbGaP_SampleAttributesDS.txt", package = "dbgaptools", mustWork = TRUE)
 
@@ -11,13 +11,13 @@ test_that("Missing DS type argument stops with error", {
 })
 
 test_that("Non-standard DS type argument stops with error",{
-   dd <- read_dd_file(ddfn)  
+   dd <- read_dd_file(ddfn)
    expect_error(.check_dd(dd, dstype="other"), "Please specify dstype, one of: pheno, ped, sattr, ssm, subj")
 })
 
 test_that("Non-uppercase column names returns a warning",{
   dd <- read_dd_file(ddfn)
-  dd$VARIABLE_TERM <- NULL  
+  dd$VARIABLE_TERM <- NULL
   names(dd)[2] <- tolower(names(dd)[2])
   expect_warning(out <- .check_dd(dd, dstype="sattr"), "All column names should be UPPER CASE", fixed=TRUE)
   expect_true(out$lowercase)
@@ -52,7 +52,7 @@ test_that("Check VALUES position other than last returns warning",{
   dd <- read_dd_file(ddfn)
   names(dd)[9] <- "VALUES"
   names(dd)[16] <- "COMMENT1"
-  dd$VARIABLE_TERM <- NULL  
+  dd$VARIABLE_TERM <- NULL
   str <- "'VALUES' must be last column"
   expect_equal(.check_dd(dd, dstype="sattr")$vals_warnings$lastcol_warn, str, fixed=TRUE)
 })
@@ -67,9 +67,9 @@ test_that("VALUES columns with multiple encodings returns warnings",{
   expect_equal(.check_dd(dd, dstype="sattr")$vals_warnings$multiple_vals_warn, str)
 
   # two encoded var
-  dd[9,15:16] <- c("1=one", "2=two, 3=three")
+  dd[9,15:16] <- list("1=one", "2=two, 3=three")
   str <- "IS_TUMOR; TUMOR_GRADE variable(s) has multiple VALUE entries per cell. Only the first entry per cell will be evaluated. Encoded values must be split into one per cell."
-  expect_equal(.check_dd(dd, dstype="sattr")$vals_warnings$multiple_vals_warn, str)  
+  expect_equal(.check_dd(dd, dstype="sattr")$vals_warnings$multiple_vals_warn, str)
 })
 
 test_that("Undefined encoded vars in DS returns warning", {
@@ -87,7 +87,7 @@ test_that("Undefined encoded vars in DS returns warning", {
 
   # two encoded vars
   ds <- ds.save
-  dd[9,15:16] <- c("G2=grade2", "G3=grade3")
+  dd[9,15:16] <- list("G2=grade2", "G3=grade3")
   str2 <- "For variable TUMOR_GRADE, the following values are undefined in the dd: G4 GX"
   expect_equal(.check_dd(dd, ds, dstype="sattr")$vals_warnings$undefined_vals_warn, str2)
 })
@@ -97,7 +97,7 @@ test_that("Difference in DS and DD variables returns warning", {
   ds <- read_ds_file(dsfn)
   ds[,"VARIABLE_TERM"] <- NULL
   dd <- read_dd_file(ddfn)
-  dd$VARIABLE_TERM <- NULL  
+  dd$VARIABLE_TERM <- NULL
 
   names(ds)[11] <- "GENOTYPING_LAB"
   expect_warning(.check_dd(dd, ds, dstype="sattr"), "Data dictionary has extra variables not in dataset: GENOTYPING_CENTER", fixed=TRUE)
@@ -111,22 +111,22 @@ test_that("DS values outside of MIN and MAX ranges warning", {
   ds <- read_ds_file(dsfn)
   dd <- read_dd_file(ddfn)
   dd$VARIABLE_TERM <- NULL
-  
+
   var <- "SEQUENCING_CENTER"
   dd$MIN[dd$VARNAME %in% var] <- 2
   dd$MAX[dd$VARNAME %in% var] <- 5
   expect_equal(.check_dd(dd, ds, dstype="sattr")$min_errors$VARNAME, "SEQUENCING_CENTER")
-  
+
   dd$MIN[dd$VARNAME %in% var] <- 0
   dd$MAX[dd$VARNAME %in% var] <- 1
-  expect_equal(.check_dd(dd, ds, dstype="sattr")$max_errors$VARNAME, "SEQUENCING_CENTER")  
+  expect_equal(.check_dd(dd, ds, dstype="sattr")$max_errors$VARNAME, "SEQUENCING_CENTER")
 })
 
 test_that("Illegal characters in variable names are reported", {
   # remove first warning
   dd <- read_dd_file(ddfn)
   dd$VARIABLE_TERM <- NULL
-  
+
   dd$VARNAME[3] <- paste0("DBGAP_",dd$VARNAME[3])
   expect_equal(.check_dd(dd, dstype="sattr")$illegal_vars, "DBGAP_ANALYTE_TYPE")
 
@@ -134,7 +134,7 @@ test_that("Illegal characters in variable names are reported", {
   expect_equal(.check_dd(dd, dstype="sattr")$illegal_vars, "ANALYTE/_TYPE")
 
   dd$VARNAME[3] <- "ANALYTE\\_TYPE"
-  expect_equal(.check_dd(dd, dstype="sattr")$illegal_vars, "ANALYTE\\_TYPE")  
+  expect_equal(.check_dd(dd, dstype="sattr")$illegal_vars, "ANALYTE\\_TYPE")
 })
 
 test_that("Missing VALUES only reported for select filetypes", {
@@ -143,21 +143,21 @@ test_that("Missing VALUES only reported for select filetypes", {
   expect_equal(.check_dd(dd, dstype="pheno")$missing_reqvars[1], "VALUES")
   expect_equal(.check_dd(dd, dstype="ped")$missing_reqvars[1], "VALUES")
   expect_equal(.check_dd(dd, dstype="sattr")$missing_reqvars[1], "VALUES")
-  expect_equal(.check_dd(dd, dstype="subj")$missing_reqvars[1], "VALUES")  
+  expect_equal(.check_dd(dd, dstype="subj")$missing_reqvars[1], "VALUES")
 })
 
 test_that("Missing UNITS only reported for select filetypes", {
   dd <- read_dd_file(ddfn)
   dd <- dd[,1:2]
   expect_equal(.check_dd(dd, dstype="pheno")$missing_reqvars[2], "UNITS")
-  expect_equal(.check_dd(dd, dstype="sattr")$missing_reqvars[2], "UNITS")  
+  expect_equal(.check_dd(dd, dstype="sattr")$missing_reqvars[2], "UNITS")
 })
 
 test_that("Unique key marked with something besides 'X' is reported", {
     dd.rev <- read_dd_file(ddfn)
     dd.rev$UNIQUEKEY[dd.rev$UNIQUEKEY %in% "X"] <- "Y"
     # get rid of this non-standard column
-    dd.rev$VARIABLE_TERM <- NULL    
+    dd.rev$VARIABLE_TERM <- NULL
     out <- .check_dd(dd.rev, dstype="sattr")
     str <- "Only 'X' should be used to flag UNIQUE key columns"
     expect_equal(out$uniquekey_flags$wrong_mark, str)
@@ -179,7 +179,7 @@ test_that("Non-unique uniquekey is reported",{
 test_that("Uniquekey for wrong file type is reportd",{
   dd.rev <- read_dd_file(ddfn)
   # get rid of this non-standard column
-  dd.rev$VARIABLE_TERM <- NULL  
+  dd.rev$VARIABLE_TERM <- NULL
   # pretend this is not a sample attributes file
   out <- .check_dd(dd.rev, dstype="subj")
   str <- "UNIQUEKEY columns(s) should not be defined for dstype subj"
